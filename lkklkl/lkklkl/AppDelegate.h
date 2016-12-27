11fdc3b5e3aea68cf52c7ac832423076bfd2ab3d
胡珈铱 <jobs@hujiayidemacbook-pro.local>
2016-12-27 11:17:59 +0800

    llkl

---
+++ lkklkl/lkklkl/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  lkklkl
+//
+//  Created by 胡珈铱 on 16/12/27.
+//  Copyright © 2016年 胡珈铱. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ lkklkl/lkklkl/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  lkklkl
+//
+//  Created by 胡珈铱 on 16/12/27.
+//  Copyright © 2016年 胡珈铱. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
